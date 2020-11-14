63f4e5a1697dfeec5651805bea6e34e6f99579de
kamiebisu <kamiebisu@protonmail.com>
2020-11-14 00:37:02 -0300

    Change to a TypeScript config, and update it

---
+++ hardhat.config.ts
@@ -0,0 +1,41 @@
+import "@nomiclabs/hardhat-waffle";
+import * as dotenv from "dotenv";
+import "hardhat-deploy";
+import {HardhatUserConfig, task} from "hardhat/config";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const mnemonic = process.env.MNEMONIC;
+
+const accounts = {
+  mnemonic,
+};
+
+const config: HardhatUserConfig = {
+  solidity: "0.7.4",
+
+  namedAccounts: {
+    deployer: 0,
+  },
+
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+      accounts,
+    },
+  },
+};
+
+export default config;
