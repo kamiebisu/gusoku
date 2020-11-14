f2730fb384488d294727778c5abf335d37da8c34
kamiebisu <kamiebisu@protonmail.com>
2020-11-14 00:37:51 -0300

    Add initial OptionsAdapter interface

---
+++ contracts/interfaces/IOptionsAdapter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+/// @title Options adapter interface
+/// @author The Gusoku contributors
+/// @notice Interface that allows uniformly interacting with different options protocols
+/// @dev Every options protocol adapter MUST implement this interface
+interface IOptionsAdapter {
+    ///@notice Query and return available PUT options
+    function getPutOptions() external view returns (address[] memory);
+
+    ///@notice Query and return available CALL options
+    // function getCallOptions() external view returns (address);
+}
