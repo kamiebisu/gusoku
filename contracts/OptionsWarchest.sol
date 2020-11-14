d26f95d7df181c0b32a57704524f37fc965d27e5
kamiebisu <kamiebisu@protonmail.com>
2020-11-14 00:39:10 -0300

    Add initial OptionsWarchest contract

---
+++ contracts/OptionsWarchest.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "./OptionsAdapter.sol";
+
+contract OptionsWarchest {
+    using OptionsAdapter for Options;
+
+    Options convexity;
+
+    constructor(address _convexity) {
+        convexity = Options(_convexity);
+    }
+
+    function getConvexityPutOptions() public view returns (address[] memory) {
+        return convexity.getPutOptions();
+    }
+}
