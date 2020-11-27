672036a9c81cef36f8882c4e91e6d888513bb405
kamiebisu <kamiebisu@protonmail.com>
2020-11-27 01:36:00 -0300

    Add OptionsModel

---
+++ contracts/adapters/domain/OptionsModel.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+library OptionsModel {
+    enum OptionType {CALL, PUT}
+
+    enum OptionMarket {CONVEXITY, HEGIC}
+
+    struct Option {
+        OptionMarket optionMarket;
+        OptionType optionType;
+        uint256 strikePrice;
+        uint256 expiryDate;
+        address tokenAddress;
+        address settlementAsset;
+        address paymentAsset;
+    }
+}
