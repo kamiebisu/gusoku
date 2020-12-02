b36c474fb16c3a3e742c4868b7b4055ea592d1c9
kamiebisu <kamiebisu@protonmail.com>
2020-12-02 04:04:56 -0300

    Start updating adapter to use the new interfaces

--- contracts/adapters/convexity/ConvexityAdapter.sol
+++ contracts/adapters/convexity/ConvexityAdapter.sol
@@ -5,16 +5,19 @@ pragma abicoder v2;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../../interfaces/IDiscreteOptionsProtocolAdapter.sol";
+import "../../interfaces/IDiscreteOptionsProtocol.sol";
+import "../../interfaces/IResellableOptionsProtocol.sol";
 import "./interfaces/IOptionsFactory.sol";
 import "./interfaces/IOptionsExchange.sol";
 import "./interfaces/IoToken.sol";
 import "../../libraries/strings.sol";
-import "hardhat/console.sol";
 import "../domain/OptionsStore.sol";
 import "../domain/OptionsModel.sol";
 
-contract ConvexityAdapter is IDiscreteOptionsProtocolAdapter, OptionsStore {
+contract ConvexityAdapter is
+    IDiscreteOptionsProtocolAdapter,
+    IResellableOptionsProtocol
+{
     using SafeMath for uint256;
     using strings for *;
 
@@ -30,69 +33,67 @@ contract ConvexityAdapter is IDiscreteOptionsProtocolAdapter, OptionsStore {
         );
     }
 
-    function _updateOptionsStore() internal {
-        uint256 numOptionsContracts = _optionsFactory
-            .getNumberOfOptionsContracts();
-        uint256 currentOptionIndex = this.currentOptionIndex();
-        if (numOptionsContracts != currentOptionIndex) {
-            for (uint256 i = currentOptionIndex; i < numOptionsContracts; i++) {
-                // Get the oToken contract
-                address oTokenAddress = _optionsFactory.optionsContracts(i);
-                IoToken oToken = IoToken(oTokenAddress);
-
-                // Differentiate between PUT and CALL options
-                bool isPut = oToken.name().toSlice().contains("Put".toSlice());
-
-                // Get the option strikePrice, handling negative exponents by considering
-                // the number of decimals of the strikeAsset
-                (uint256 value, int32 exponent) = oToken.strikePrice();
-                address strikeAddress = oToken.strike();
-                uint8 decimals;
-                if (strikeAddress == address(0)) {
-                    // strike is ETH
-                    decimals = 18;
-                } else {
-                    ERC20 strikeAsset = ERC20(oToken.strike());
-                    decimals = strikeAsset.decimals();
-                }
-                uint256 strikePrice = value * 10**uint256(exponent + decimals);
-
-                // Create an Option struct and save it to the OptionsStore
-                createOption(
-                    OptionsModel.OptionMarket.CONVEXITY,
-                    isPut
-                        ? OptionsModel.OptionType.PUT
-                        : OptionsModel.OptionType.CALL,
-                    strikePrice,
-                    oToken.expiry(),
-                    oTokenAddress,
-                    strikeAddress,
-                    address(0) // Default to ETH for now
-                );
-            }
+    ///@notice Get the option strikePrice
+    ///@dev Handles negative exponents by considering the number of decimals of the strikeAsset
+    function _getStrikePrice(IoToken oToken)
+        internal
+        view
+        returns (uint256 strikePrice)
+    {
+        (uint256 value, int32 exponent) = oToken.strikePrice();
+        address strikeAddress = oToken.strike();
+        uint8 decimals;
+        if (strikeAddress == address(0)) {
+            // strike is ETH
+            decimals = 18;
+        } else {
+            ERC20 strikeAsset = ERC20(oToken.strike());
+            decimals = strikeAsset.decimals();
         }
+        strikePrice = value * 10**uint256(exponent + decimals);
     }
 
-    function _getFilteredOptions(OptionsModel.OptionType _optionType)
-        internal
-        returns (OptionsModel.Option[] memory)
-    {
-        // First, update the options cache if necessary
-        _updateOptionsStore();
+    function _getFilteredOptions(
+        OptionsModel.OptionType _optionType,
+        address baseAsset
+    ) internal view returns (OptionsModel.Option[] memory) {
+        uint256 numOptionsContracts = _optionsFactory
+            .getNumberOfOptionsContracts();
 
         OptionsModel.Option[] memory currentOptions = new OptionsModel.Option[](
-            currentOptionIndex
+            numOptionsContracts
         );
         uint256 numFilteredOptions = 0;
-        for (uint256 i = 0; i < currentOptionIndex; i++) {
-            if (options[i].optionType == _optionType) {
-                currentOptions[numFilteredOptions] = options[i];
+        for (uint256 i = 0; i < numOptionsContracts; i++) {
+            address oTokenAddress = _optionsFactory.optionsContracts(i);
+            oToken = IoToken(oTokenAddress);
+            string memory filter = _optionType == OptionsModel.OptionType.PUT
+                ? "Put"
+                : "Call";
+
+            if (
+                !oToken.hasExpired() &&
+                ((_optionType == OptionsModel.OptionType.PUT &&
+                    oToken.underlying() == baseAsset) ||
+                    (_optionType == OptionsModel.OptionType.CALL &&
+                        oToken.strike() == baseAsset)) &&
+                (oToken.name().toSlice().contains(filter.toSlice()) ||
+                    oToken.symbol().toSlice().contains(filter.toSlice()))
+            ) {
+                currentOptions[numFilteredOptions] = OptionsModel.Option({
+                    optionMarket: OptionsModel.OptionMarket.CONVEXITY,
+                    optionType: _optionType,
+                    strikePrice: _getStrikePrice(oToken),
+                    expiryDate: oToken.expiry(),
+                    tokenAddress: oTokenAddress,
+                    settlementAsset: oToken.strike(),
+                    paymentAsset: address(0)
+                });
                 numFilteredOptions = numFilteredOptions.add(1);
             }
         }
 
         // Trim array removing zero addresses, just for simplicity when it's consumed.
-        // No gas is saved anyways.
 
 
             OptionsModel.Option[] memory filteredOptions
@@ -105,22 +106,22 @@ contract ConvexityAdapter is IDiscreteOptionsProtocolAdapter, OptionsStore {
         return filteredOptions;
     }
 
-    function getPutOptions()
+    function getPutOptions(address baseAsset)
         external
         view
         override
         returns (OptionsModel.Option[] memory)
     {
-        return _getFilteredOptions(OptionsModel.OptionType.PUT);
+        return _getFilteredOptions(OptionsModel.OptionType.PUT, baseAsset);
     }
 
-    function getCallOptions()
+    function getCallOptions(address baseAsset)
         external
         view
         override
         returns (OptionsModel.Option[] memory)
     {
-        return _getFilteredOptions(OptionsModel.OptionType.CALL);
+        return _getFilteredOptions(OptionsModel.OptionType.CALL, baseAsset);
     }
 
     function getPrice(
