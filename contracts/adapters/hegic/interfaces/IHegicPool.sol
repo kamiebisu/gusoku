24a8fdd54614765e64fc6eeef2d491230a344888
kamiebisu <kamiebisu@protonmail.com>
2021-01-11 23:37:47 -0300

    Remove old files

--- contracts/adapters/hegic/interfaces/IHegicPool.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity >=0.5.0 <0.8.0;
-pragma experimental ABIEncoderV2;
-
-interface IHegicPool {
-    function availableBalance() external view returns (uint256 balance);
-
-    function totalBalance() external view returns (uint256 balance);
-}
